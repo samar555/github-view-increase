2c631b085d07d3f8f663c8a40a434635cc2081ef
samar555 <sachinmishraravi@gmail.com>
2025-04-18 13:33:02 +0530

    added angular and build home page

---
+++ githubViewIncreser/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./core/header/header.component";
+import { FooterComponent } from "./core/footer/footer.component";
+import { SectionComponent } from "./core/section/section.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, SectionComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'githubViewIncreser';
+}
